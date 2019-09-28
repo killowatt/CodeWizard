cc591383c171fc0de6da00b58ab1dddaa6f81033
William Yates <killowatt2213@gmail.com>
2019-09-28 01:31:29 -0400

    Fixed scene collision, added updated player movement code

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D bc;
 
     float speed = 14.0f;
     float smoothing = 0.05f;
 
-    private bool grounded;
+    public bool grounded;
+    public bool flipped = false;
 
-    public Transform grdchek; // pos to check ground
 
     public LayerMask groundLayer;
 
@@ -19,21 +20,25 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
     }
     void FixedUpdate()
     {
         //float groundRad = 0.2f;
 
+        //grounded = Physics2D.Raycast(transform.position, Vector2.down, 1.05f, groundLayer).collider;
+        grounded = Physics2D.OverlapBox(transform.position + Vector3.down, new Vector2(0.8f, 0.05f), 0.0f, groundLayer);
+        Debug.DrawRay(transform.position, Vector2.down * 1.05f);
         // BORROWED CODE ##################################
         //bool wasGrounded = grounded;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.down * 1.5f, Vector2.down, 0.05f);
-        if (hit.collider != null)
-        {
-            grounded = true;
-        }
-        else
-            grounded = false;
-            //// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        //RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.down * 1.0f, Vector2.down, 0.001f);
+        //if (hit.collider != null)
+        //{
+        //    grounded = true;
+        //}
+        //else
+        //    grounded = false;
+        //// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
         //// This can be done using layers instead but Sample Assets will not overwrite your project settings.
         //Collider2D[] colliders = Physics2D.OverlapCircleAll(grdchek.position, groundRad);
         //for (int i = 0; i < colliders.Length; i++)
@@ -48,7 +53,16 @@ public class Player : MonoBehaviour
         //// BORROWED CODE#####################################
 
         float horizontalInput = Input.GetAxis("Horizontal");
-
+        if (horizontalInput > 0.0f && flipped)
+        {
+            transform.localScale = transform.localScale * new Vector2(-1.0f, 1.0f);
+            flipped = false;
+        }
+        else if (horizontalInput < 0.0f && !flipped)
+        {
+            transform.localScale = transform.localScale * new Vector2(-1.0f, 1.0f);
+            flipped = true;
+        }
 
         Vector2 velo = Vector2.zero;
 
@@ -58,12 +72,13 @@ public class Player : MonoBehaviour
 
         rb.velocity = Vector2.SmoothDamp(rb.velocity,
             targetVelocity, ref velo, smoothing);
+        //rb.AddForce(new Vector2(horizontalInput, 0.0f))
 
         // Jump
         if (grounded && Input.GetKey(KeyCode.W))
         {
             grounded = false;
-            rb.AddForce(new Vector2(0, 100.0f));
+            rb.AddForce(new Vector2(0, 750.0f));
         }
         //float horizontal = Input.GetAxis("Horizontal");
         //float vertical = 0.0f;
