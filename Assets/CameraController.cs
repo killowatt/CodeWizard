3e9df58aa7b40c6d7bbfa9a21c4d0830d9fda8ca
William Yates <killowatt2213@gmail.com>
2019-09-28 01:52:06 -0400

    Add new tiles, player camera follows player

---
+++ Assets/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 t = Player.transform.position;
+        t.z = -10;
+        transform.position = t;
+    }
+}
