bfa90868879eee069bf5f4b86861085f2ef7e939
William Yates <killowatt2213@gmail.com>
2019-09-27 22:25:54 -0400

    Add playertest scene

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
 
+        rb.AddForce(new Vector2(horizontal * 5.0f, 0.0f));
+    }
     // Update is called once per frame
     void Update()
     {
