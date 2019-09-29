2c42befccb4a6a96965d8f217de16d231df29bf9
William Yates <killowatt2213@gmail.com>
2019-09-29 01:31:17 -0700

    Add terminal and platform graphics and placement

---
+++ Assets/PlatformController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MoonSharp.Interpreter;
+
+[MoonSharpUserData]
+public class PlatformCode
+{
+
+}
+public class PlatformController : CodeSystem
+{
+    public float UpperLimitY;
+    public float LowerLimitY;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
