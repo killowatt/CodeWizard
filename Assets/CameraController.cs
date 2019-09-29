8d0d8478b6b731f747f6fba998089974f8cd1223
William Yates <killowatt2213@gmail.com>
2019-09-29 03:33:31 -0700

    Camera smoothing implementation, graphics update

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -6,6 +6,7 @@ public class CameraController : MonoBehaviour
 {
     public GameObject Player;
 
+    public float Smoothing = 0.05f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,16 @@ public class CameraController : MonoBehaviour
     {
         Vector3 t = Player.transform.position;
         t.z = -10;
-        transform.position = t;
+
+        Vector3 velocity = Vector3.zero;
+        Vector3 desired = Vector3.SmoothDamp(transform.position, t, ref velocity, Smoothing);
+
+        desired *= 16.0f;
+        desired.x = Mathf.Round(desired.x);
+        desired.y = Mathf.Round(desired.y);
+        //desired.z = Mathf.Round(desired.z);
+        desired /= 16.0f;
+
+        transform.position = desired;
     }
 }
